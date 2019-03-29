e34e4d8edfe05320889b68a7b8ea9000bd79a123
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-29 12:08:48 -0400

    Populated actions helper file

---
+++ models/actions-model.js
@@ -0,0 +1,21 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  getActions,
+  addAction,
+  getActionsByProject,
+};
+
+function addAction(action) {
+  return db("actions")
+    .insert(action);
+}
+
+function getActions() {
+  return db("actions");
+}
+
+function getActionsByProject(id) {
+  return db("actions")
+    .where({ project_id: id })
+}
