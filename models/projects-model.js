0a2f836f3bbffdc2b96837f1aa93eb224b49e39e
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-29 12:05:40 -0400

    Created projects helper file

---
+++ models/projects-model.js
@@ -0,0 +1,22 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  addProjects,
+  getProjects,
+  getProject,
+};
+
+function addProjects(project) {
+  return db("projects")
+    .insert(project);
+}
+
+function getProjects() {
+  return db("projects");
+}
+
+function getProject(id) {
+  return db("projects")
+    .where({ id })
+    .first();
+}
