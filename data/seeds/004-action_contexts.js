f58be1864fdcb2ad216701400272d86f951deafc
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-29 13:02:37 -0400

    Updated action_contexts seed file

--- data/seeds/004-action_contexts.js
+++ data/seeds/004-action_contexts.js
@@ -1,14 +1,13 @@
 exports.seed = function(knex, Promise) {
   return knex('recipe_ingredients').insert([
     { action_id: 1, context_id: 1 }, // 1
-    { action_id: 1, context_id: 2 }, // 2
-    { action_id: 5, context_id: 1 }, // 3
-    { action_id: 5, context_id: 3 }, // 5
-    { action_id: 7, context_id: 1 }, // 6
-    { action_id: 7, context_id: 2 }, // 7
-    { action_id: 10, context_id: 1 }, // 8
-    { action_id: 10, context_id: 2 }, // 9
-    { action_id: 10, context_id: 3 }, // 10
-    { action_id: 10, context_id: 4 }, // 11
+    { action_id: 2, context_id: 3 }, // 2
+    { action_id: 3, context_id: 3 }, // 3
+    { action_id: 4, context_id: 1 }, // 5
+    { action_id: 5, context_id: 1 }, // 6
+    { action_id: 6, context_id: 3 }, // 7
+    { action_id: 7, context_id: 3 }, // 8
+    { action_id: 8, context_id: 3 }, // 9
+    { action_id: 9, context_id: 3 }, // 10
   ]);
 };
