1e9d284480f969f462d2569a630a1c7c16bf7e6a
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-03-29 12:55:16 -0400

    Deleted DB and updated migration file to include contexts

--- data/migrations/20190329111313_first_draft.js
+++ data/migrations/20190329111313_first_draft.js
@@ -27,13 +27,48 @@ exports.up = function(knex, Promise) {
 
       tbl.text("description").notNullable();
 
-      tbl.text("notes")
+      tbl.text("notes");
 
       tbl.boolean("complete").defaultTo(false);
+    })
+
+    .createTable("contexts", tbl => {
+      tbl.increments();
+
+      tbl
+        .string("name", 128)
+        .notNullable()
+        .unique();
+    })
+
+    .createTable("action_contexts", tbl => {
+      tbl.increments();
+
+      tbl
+        .integer("action_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("actions")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+
+      tbl
+        .integer("context_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("contexts")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
     });
 };
 
 exports.down = function(knex, Promise) {
   // tables with FK must be removed before the referenced table is removed
-  return knex.schema.dropTableIfExists("actions").dropTableIfExists("projects");
+  return knex.schema
+    .dropTableIfExists("action_contexts")
+    .dropTableIfExists("contexts")
+    .dropTableIfExists("actions")
+    .dropTableIfExists("projects");
 };
